1846d8119fab73339ae5b2c1406dd1b8040388fd
Teddy Ortega <tortega128@gmail.com>
2023-12-07 07:53:14 -0600

    org

--- lib/util.ml
+++ lib/util.ml
@@ -1,11 +1,21 @@
+(* string *)
 let trim_and_split_on_char c s = s |> String.trim |> String.split_on_char c
 let explode s = s |> String.to_seq |> List.of_seq
+let split_on_whitespace str = Str.split (Str.regexp "[ ]+") str
+
+let%test "split_on_whitespace" =
+  split_on_whitespace "hi   hello cool       beans"
+  = [ "hi"; "hello"; "cool"; "beans" ]
+;;
+
+(* char *)
 let is_digit c = Char.code c >= Char.code '0' && Char.code c <= Char.code '9'
+
+(* comparison *)
 let max_with_string i s = s |> int_of_string |> max i
 let max_with_strings s1 s2 = max (int_of_string s1) (int_of_string s2)
-let split_on_whitespace str = Str.split (Str.regexp "[ ]+") str
-let%test _ = split_on_whitespace "hi   hello" = [ "hi"; "hello" ]
 
+(* debugging *)
 let print_int_list lst () =
   List.iter (fun x -> Printf.printf "%d, " x) lst;
   Printf.printf "\n"
